61c5b1a8c65d2406132f4d9b5d0510795b455644
Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>
2025-04-11 18:26:34 +0200

    added search

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,32 @@
-export default function PostShowPage() {
-  return <div>Post Show Page</div>
+import { Suspense } from 'react'
+import Link from 'next/link'
+import PostShow from '@/components/posts/post-show'
+import PostShowLoading from '@/components/posts/post-show-loading'
+import CommentList from '@/components/comments/comment-list'
+import CommentCreateForm from '@/components/comments/comment-create-form'
+import paths from '@/paths'
+import { fetchCommentsByPostId } from '@/db/queries/comments'
+
+interface PostShowPageProps {
+  params: Promise<{
+    slug: string
+    postId: string
+  }>
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = await params
+
+  return (
+    <div className='space-y-3'>
+      <Link className='underline decoration-solid' href={paths.topicShow(slug)}>
+        {'< '}Back to {slug}
+      </Link>
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
+    </div>
+  )
 }
